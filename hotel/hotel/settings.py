d2352c375dc934009c118937b84469d63d42b547
Ilnur <bcarlee@student.21-school.ru>
2022-02-02 12:43:57 +0300

    Update settings.py

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tm^+gaa9z*j1p#2kib!ly64_u3za8gtftbjk)m5o(x645%=9@%'
+SECRET_KEY = 'YOUR_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
